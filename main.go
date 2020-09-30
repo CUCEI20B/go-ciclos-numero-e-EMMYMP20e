bed6a9f57a75bec1f18d54f4695a7f8fb4ae03e3
Emmanuel Méndez <emmy.mp.98e@gmail.com>
2020-09-29 19:55:39 -0500

    numero e

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var e float64
+	var n float64
+	var fact float64
+
+	var x float64
+
+	fmt.Scan(&x)
+
+	e = 0
+	fact = 1
+	n = 0
+	for n < x {
+		e += 1 / (fact)
+		n++
+		fact *= n
+	}
+	fmt.Println(e)
 }
