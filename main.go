38f1319fabb76ab7e0ede444e38fa5851b36bbfd
Emmanuel Méndez <emmy.mp.98e@gmail.com>
2020-09-29 21:03:47 -0500

    correccion

--- main.go
+++ main.go
@@ -3,18 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	var e float64
-	var n float64
-	var fact float64
-
 	var x float64
 
 	fmt.Scan(&x)
 
-	e = 0
-	fact = 1
-	n = 0
-	for n < x {
+	e := 0.0
+	fact := 1.0
+	n := 0.0
+	for n <= x {
 		e += 1 / (fact)
 		n++
 		fact *= n
